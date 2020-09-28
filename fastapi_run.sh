ffc12213b1ad96388345067ee02836fc394f22ed
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-09-28 17:21:44 +0200

    Fixed index page and startup file

--- fastapi_run.sh
+++ fastapi_run.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 # From the root of the repository, type `python flask_run.py` to start the flask server
-uvicorn fastapi_app.webapp:app --reload --port 5001
+uvicorn oedatamodel_api.webapp:app --reload --port 5001
--- oedatamodel_api/webapp.py
+++ oedatamodel_api/webapp.py
@@ -1,20 +1,20 @@
 import os
+
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 
 app = FastAPI()
 
 SERVER_ROOT = os.path.dirname(__file__)
 
 app.mount(
-    "/static", StaticFiles(directory=os.path.join(SERVER_ROOT, "static")), name="static"
+    '/static', StaticFiles(directory=os.path.join(SERVER_ROOT, 'static')), name='static',
 )
 
-templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
+templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, 'templates'))
 
 
-@app.get("/")
+@app.get('/')
 def index(request: Request) -> Response:
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse('index.html', {'request': request})
