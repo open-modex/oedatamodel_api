8d1891d8f0b2fc8f2573af587ff098150744f3d9
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-09-29 10:24:17 +0200

    Implemented scenario raw data request

---
+++ oedatamodel_api/oep_connector.py
@@ -0,0 +1,115 @@
+
+import logging
+import requests
+
+OEP_URL = 'https://openenergy-platform.org'
+
+
+def get_scenario_response_from_oep(scenario_id=None, scenario_name=None):
+    if scenario_id is None and scenario_name is None:
+        raise ValueError("You have to set either scenario ID or name")
+    if scenario_id is not None and scenario_name is not None:
+        raise ValueError("Scenario name and ID given.")
+    if scenario_id is not None:
+        where_clause = {
+            "operands": [
+                {
+                    "type": "column",
+                    "table": "s",
+                    "column": "id",
+                },
+                scenario_id,
+            ],
+            "operator": "=",
+            "type": "operator",
+        }
+    else:
+        where_clause = {
+            "operands": [
+                {
+                    "type": "column",
+                    "table": "s",
+                    "column": "scenario",
+                },
+                scenario_name,
+            ],
+            "operator": "=",
+            "type": "operator",
+        }
+    join = {
+        "from": {
+            "type": "join",
+            "left": {
+                "type": "table",
+                "table": "oed_scenario",
+                "schema": "model_draft",
+                "alias": "s",
+            },
+            "right": {
+                "type": "join",
+                "is_full": True,
+                "left": {
+                    "type": "join",
+                    "is_full": True,
+                    "left": {
+                        "type": "table",
+                        "table": "oed_data",
+                        "schema": "model_draft",
+                        "alias": "d",
+                    },
+                    "right": {
+                        "type": "table",
+                        "table": "oed_timeseries",
+                        "schema": "model_draft",
+                        "alias": "ts",
+                    },
+                    "on": {
+                        "operands": [
+                            {"type": "column", "column": "id", "table": "d"},
+                            {"type": "column", "column": "id", "table": "ts"},
+                        ],
+                        "operator": "=",
+                        "type": "operator",
+                    },
+                },
+                "right": {
+                    "type": "table",
+                    "table": "oed_scalar",
+                    "schema": "model_draft",
+                    "alias": "sc",
+                },
+                "on": {
+                    "operands": [
+                        {"type": "column", "column": "id", "table": "d"},
+                        {"type": "column", "column": "id", "table": "sc"},
+                    ],
+                    "operator": "=",
+                    "type": "operator",
+                },
+            },
+            "on": {
+                "operands": [
+                    {"type": "column", "column": "id", "table": "s"},
+                    {"type": "column", "column": "scenario_id", "table": "d"},
+                ],
+                "operator": "=",
+                "type": "operator",
+            },
+        },
+        "where": where_clause,
+    }
+    data = {'query': join}
+    response = requests.post(
+        OEP_URL + '/api/v0/advanced/search',
+        json=data,
+    )
+    if response.status_code != 200:
+        logging.error("Could not get scenario from OEP", scenario_id, scenario_name, response.text)
+        raise ConnectionError(response.text)
+    return response.json()
+
+
+def get_scenario_data(scenario_json: dict):
+    if scenario_json["content"]["rowcount"] == 0:
+        return {"result": "Scenario not found"}
+    return scenario_json['data']
--- oedatamodel_api/webapp.py
+++ oedatamodel_api/webapp.py
@@ -1,8 +1,11 @@
 import os
+import uvicorn
 
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from oedatamodel_api.oep_connector import get_scenario_response_from_oep, get_scenario_data
 
 app = FastAPI()
 
@@ -11,10 +14,25 @@ SERVER_ROOT = os.path.dirname(__file__)
 app.mount(
     '/static', StaticFiles(directory=os.path.join(SERVER_ROOT, 'static')), name='static',
 )
-
 templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, 'templates'))
 
 
 @app.get('/')
 def index(request: Request) -> Response:
     return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/scenario/id/{scenario_id}')
+def scenario_by_id(scenario_id: int):
+    scenario_json = get_scenario_response_from_oep(scenario_id=scenario_id)
+    return get_scenario_data(scenario_json)
+
+
+@app.get('/scenario/name/{scenario_name}')
+def scenario_by_name(scenario_name: str):
+    scenario_json = get_scenario_response_from_oep(scenario_name=scenario_name)
+    return get_scenario_data(scenario_json)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
