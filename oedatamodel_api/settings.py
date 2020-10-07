4ae04d60e7ab28e78fa49446d911a7bdec81f0a8
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-10-07 17:08:48 +0200

    Resolved ROOT_DIR to absolute path

--- oedatamodel_api/settings.py
+++ oedatamodel_api/settings.py
@@ -1,5 +1,5 @@
 
 import pathlib
 
-ROOT_DIR = pathlib.Path(__file__).parent.parent
+ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
 MAPPINGS_DIR = ROOT_DIR / "oedatamodel_api" / "mappings"
