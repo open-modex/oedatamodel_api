6da34e0eacd02b949a14d19dfee1129003477f9c
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-10-07 17:44:07 +0200

    Implemented nested zipfiles for csv output format

--- oedatamodel_api/formatting.py
+++ oedatamodel_api/formatting.py
@@ -1,5 +1,6 @@
 import csv
 import zipfile
+import pathlib
 from enum import Enum
 from io import BytesIO, StringIO
 
@@ -10,10 +11,19 @@ class OutputFormat(str, Enum):
     csv = 'csv'
 
 
-def create_zip_csv(data):
+def create_zip_csv(json_data):
     zipped_file = BytesIO()
+    root_dir = pathlib.Path()
     with zipfile.ZipFile(zipped_file, 'a', zipfile.ZIP_DEFLATED) as zipped:
-        for name, data in data.items():
+        _iterate_zip_dirs(zipped, json_data, root_dir)
+    zipped_file.seek(0)
+    return zipped_file
+
+
+def _iterate_zip_dirs(zipped, json_data, current_dir):
+    for name, data in json_data.items():
+        # Check if current node is directory or data for csv:
+        if isinstance(data, list) or not all(isinstance(v, dict) or isinstance(v, list) for v in data.values()):
             csv_data = StringIO()
             writer = csv.writer(csv_data, delimiter=',')
             if isinstance(data, dict):
@@ -27,6 +37,9 @@ def create_zip_csv(data):
                 raise TypeError('Unknown type to create csv from')
             csv_data.seek(0)
             csv_buffer = csv_data.read()
-            zipped.writestr(f'{name}.csv', csv_buffer)
-    zipped_file.seek(0)
-    return zipped_file
+            if current_dir == pathlib.Path():
+                zipped.writestr(f'{name}.csv', csv_buffer)
+            else:
+                zipped.writestr(f'{current_dir}/{name}.csv', csv_buffer)
+        else:
+            _iterate_zip_dirs(zipped, data, current_dir / name)
