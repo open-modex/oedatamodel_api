aac6cfc68cbaaa549242a2a0f13ac6c34aa3214d
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-11-30 09:33:56 +0100

    Implemented dynamical sources for OEP requests

--- oedatamodel_api/webapp.py
+++ oedatamodel_api/webapp.py
@@ -1,12 +1,13 @@
+
 import uvicorn
-import json
+from functools import lru_cache
 
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from oedatamodel_api.oep_connector import get_scenario_from_oep, ScenarioNotFoundError
+from oedatamodel_api.oep_connector import get_data_from_oep, OEPDataNotFoundError
 from oedatamodel_api import mapping_custom, formatting
 from oedatamodel_api.settings import ROOT_DIR
 
@@ -45,30 +46,35 @@ def prepare_response(raw_json, mapping, output_format):
         return mapped_data
 
 
+@lru_cache(maxsize=None)
+def load_source(source, **params):
+    try:
+        raw_data = get_data_from_oep(source, params)
+    except (ConnectionError, OEPDataNotFoundError) as e:
+        return {"error": e.args}
+    return raw_data
+
+
 @app.get('/scenario/id/{scenario_id}')
 def scenario_by_id(
     scenario_id: int,
+    source: str,
     mapping: str,
     output: formatting.OutputFormat = formatting.OutputFormat.json
 ):
-    try:
-        raw_scenario_json = get_scenario_from_oep(scenario_id=scenario_id)
-    except (ConnectionError, ScenarioNotFoundError) as e:
-        return {"error": e.args}
-    return prepare_response(raw_scenario_json, mapping, output)
+    raw_data = load_source(source, scenario_id=scenario_id)
+    return prepare_response(raw_data, mapping, output)
 
 
 @app.get('/scenario/name/{scenario_name}')
 def scenario_by_name(
     scenario_name: str,
+    source: str,
     mapping: str,
     output: formatting.OutputFormat = formatting.OutputFormat.json
 ):
-    try:
-        raw_scenario_json = get_scenario_from_oep(scenario_name=scenario_name)
-    except (ConnectionError, ScenarioNotFoundError) as e:
-        return {"error": e.args}
-    return prepare_response(raw_scenario_json, mapping, output)
+    raw_data = load_source(source, scenario_name=scenario_name)
+    return prepare_response(raw_data, mapping, output)
 
 
 if __name__ == "__main__":
