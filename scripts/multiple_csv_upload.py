de006c823dc505e1b9e32f53539a6e6884f20f58
FelixMau <fmau@posteo.de>
2023-03-13 13:22:28 +0100

    moved folder

---
+++ scripts/multiple_csv_upload.py
@@ -0,0 +1,28 @@
+import os
+import pathlib
+
+import requests
+
+MODEX_URL = "https://modex.rl-institut.de/upload/"
+
+path = pathlib.Path(
+    "/home/local/RL-INSTITUT/felix.maurer/rli/Felix.Maurer/SEDOS/Python/data_adapter_oemof/tests"
+    "/_files/tabular_datapackage_mininmal_example_collection/csvs"
+)
+
+CSVS = list(path.iterdir())
+
+TOKEN = os.environ["TOKEN"]
+
+
+for file in CSVS:
+    response = requests.post(
+        MODEX_URL,
+        data={
+            "schema": "model_draft",
+            "token": TOKEN,
+            "table": pathlib.Path(file).name.split(".")[0],
+        },
+        files=[("csv_file", (pathlib.Path(file).name, open(file, "rb")))],
+    )
+    print(response)
