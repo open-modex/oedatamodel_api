f054adf078f60499e9bfa8c40c8ed8985cafeb8a
FelixMau <fmau@posteo.de>
2023-03-15 13:01:58 +0100

    Adjust folder structure

--- scripts/environ_setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ["TOKEN"] = "35fbc60fe3035bbdcf4d72fea497a88192e6eca6"
-os.environ["OEP_USER"] = "Felix.Maurer"
-os.environ["API_KEY"] = "5b7c7e62-dd72-4b4f-923c-56d408178a59"
-os.environ["account_name"] = "felixmaur"
-os.environ["group"] = "minimal_example"
-os.environ["version"] = "v3"
--- scripts/multiple_databus_registration.py
+++ scripts/multiple_databus_registration.py
@@ -1,7 +1,6 @@
 import os
 import pathlib
 
-import environ_setup
 
 import requests
 
