290a222943b9a1376223c9698fbccf03dc4d3d90
FelixMau <fmau@posteo.de>
2023-03-15 13:13:39 +0100

    pre-commit

--- scripts/data_OEP_registration.py
+++ scripts/data_OEP_registration.py
@@ -7,7 +7,9 @@ import requests
 
 MODEX_URL = "https://modex.rl-institut.de/create_table/"
 
-path = pathlib.Path("/home/local/RL-INSTITUT/felix.maurer/rli/Felix.Maurer/SEDOS/Python/data_adapter_oemof/tests/_files/tabular_datapackage_mininmal_example_collection/json"
+path = pathlib.Path(
+    "/home/local/RL-INSTITUT/felix.maurer/rli/Felix.Maurer/SEDOS/Python/data_adapter_oemof/tests/"
+    "_files/tabular_datapackage_mininmal_example_collection/json"
 )
 
 JSONS = list(path.iterdir())
@@ -23,7 +25,7 @@ for file in JSONS:
         data={
             "user": os.environ["OEP_USER"],
             "token": os.environ["TOKEN"],
-               },
+        },
         files=[("metadata_file", (pathlib.Path(file).name, open(file, "rb")))],
     )
     print(response)
